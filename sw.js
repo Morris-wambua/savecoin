1d20abb3f916c0f01fc766c364a4f2927b0f1bcf
Morris Kioko <45072943+Morris-wambua@users.noreply.github.com>
2026-08-31 23:55:13 +0300

    Refresh the PWA cache
    Bump the service-worker cache so installed copies receive the loan-flow fix.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='savecoin-v1.3.1';
+const CACHE='savecoin-v1.3.2';
 const ASSETS=['./','./index.html','./app.css','./app.js','./config.js','./db.js','./finance.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
